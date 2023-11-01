ca08b87ec8cb53d2d97e64831e1cea91e3b5fdc0
Zulpa Agustin <113565616+ZulpaAgustin@users.noreply.github.com>
2023-11-01 16:40:16 +0700

    Delete nuxt.config.ts

--- nuxt.config.ts
+++
@@ -1,6 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['@Nuxtjs/supabase'],
-  supabase: {redirect: false},
-})
